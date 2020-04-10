b3d9bda741db118630100ee0f0dd1b822b3d18ce
ckopecky <kopecky12112@gmail.com>
2020-04-09 18:34:31 -0700

    knexfile added

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+
+export const environment = {
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: './data/moviereviews.sqlite3'
+        },
+        useNullAsDefault: true,
+        migrations: { 
+            directory: './data/migrations/development' 
+        },
+        seeds: { 
+            directory: './data/seeds/development' 
+        }
+    },
+    
+    production: {
+        client: 'pg', 
+        connection: process.env.DATABASE_URL,
+        useNullAsDefault: true,
+            pool: {
+                min: 2,
+                max: 10
+            },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: './data/migrations/development'
+        },
+        seeds: { 
+            directory: './data/seeds/production' 
+        }
+    }
+};
