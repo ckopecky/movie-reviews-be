71dd0fcbd1eb0c3bff85662620690cfe23d0c935
ckopecky <kopecky12112@gmail.com>
2020-04-10 01:14:34 -0700

    users table done

--- src/data/migrations/development/20200409221353_users.js
+++ src/data/migrations/development/20200409221353_users.js
@@ -1,8 +1,39 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schemas.createTable('users', users => {
+        users
+            .increments('id')
+        users
+            .string('twitterId', 25)
+            .unique()
+        users
+            .string('googleId', 50)
+            .unique()
+        users
+            .string('stripeId', 50)
+            .unique()
+        users
+            .string('username', 50)
+            .unique()
+        users
+            .string('name', 100)
+        users
+            .string('email', 254)
+            .unique()
+        users
+            .boolean('premium_user')
+            .defaultTo(false)
+        users
+            .string('photo', 200)
+        users
+            .timestamp('created_at')
+            .defaultTo(knex.fn.now())
+        users
+            .timestamp('updated_at')
+            .defaultTo(knex.fn.now())
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('users');
 };
