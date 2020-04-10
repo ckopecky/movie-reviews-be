2c12a9529033f2ad82d79acda1afca3d86db2055
ckopecky <kopecky12112@gmail.com>
2020-04-10 02:12:19 -0700

    paymentRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "dotenv/config";
 import express from 'express';
 import { middleware } from "./api/configureMiddleware";
+import './services/passport';
+
+import userRouter from './api/users/usersRouter';
+import authRouter from './api/authentication/authRouter';
+import authReviewRouter from './api/reviews/authReviewsRouter';
+import reviewsRouter from './api/reviews/reviewsRouter';
+import paymentRouter from './api/payments/paymentRouter';
+
+const port = process.env.PORT || 5000;
+
 
 
