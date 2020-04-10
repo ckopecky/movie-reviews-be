0f17108d81faf4cdfe8a8615c5651f69ce6c266c
ckopecky <kopecky12112@gmail.com>
2020-04-10 01:09:39 -0700

    movieReviews table created

--- src/data/migrations/development/20200409221407_movieReviews.js
+++ src/data/migrations/development/20200409221407_movieReviews.js
@@ -1,8 +1,39 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('movieReviews', movieReviews => {
+        movieReviews.increments('id');
+        movieReviews
+            .integer('movieId')
+            .notNullable();
+        movieReviews
+            .integer('userId')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('cascade')
+            .index();
+        movieReviews
+            .string('reviewer')
+            .notNullable()
+            .references('email')
+            .inTable('users')
+            .onDelete('cascade')
+            .index()
+        movieReviews
+            .text('textBody', 5000)
+            .notNullable()
+        movieReviews
+            .integer('rating')
+        movieReviews
+            .timestamp('created_at')
+            .defaultTo(knex.fn.now())
+        movieReviews
+            .timestamp('updated_at')
+            .defaultTo(knex.fn.now())
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('movieReviews');
 };
