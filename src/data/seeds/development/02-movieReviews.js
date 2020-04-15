44e0405175bfe97cf519852c23b4769777849aac
ckopecky <kopecky12112@gmail.com>
2020-04-14 21:11:12 -0700

    update seed files

--- src/data/seeds/development/02-movieReviews.js
+++ src/data/seeds/development/02-movieReviews.js
@@ -2,10 +2,10 @@ const faker = require('faker');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('movieReviews').del()
+  return knex('moviereviews').del()
       .then(() => {
         // Inserts seed entries
-        let movieReviews = [];
+        let moviereviews = [];
         let movieIds = [8587, 5156, 992, 278, 238, 424, 372058, 496243, 129, 680, 637, 13, 122, 769, 389, 346, 510,508439, 446893, 522212, 571265, 419704, 619264, 181812, 512200, 408647, 102899, 571, 11113, 11, 762, 408, 601, 8810, 812, 10693, 329, 770 ]
         for(let i = 0; i <= 100; i++) {
           let movieId = movieIds[Math.floor(Math.random() * movieIds.length)];
@@ -13,8 +13,8 @@ exports.seed = function(knex) {
           let reviewer = 'kopecky12112@gmail.com';
           let textBody = faker.lorem.paragraph(7);
           let rating = faker.random.number({min: 1, max: 5});
-          movieReviews.push({movieId, userId, reviewer, textBody, rating});
+          moviereviews.push({movieId, userId, reviewer, textBody, rating});
         }
-        return knex('movieReviews').insert(movieReviews);
+        return knex('moviereviews').insert(moviereviews);
       });
 };
